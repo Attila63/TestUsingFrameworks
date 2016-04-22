e7dc1deb8c3a138869d935f3a2fdba5c7b272c8c
Igor Gun <igor.gun@idealo.de>
2016-04-22 10:12:30 +0200

    Initial Commit

---
+++ TestUsingFrameworks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUsingFrameworks
+//
+//  Created by Igor Gun on 22.04.16.
+//  Copyright © 2016 Igor Gun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestUsingFrameworks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestUsingFrameworks
+//
+//  Created by Igor Gun on 22.04.16.
+//  Copyright © 2016 Igor Gun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
